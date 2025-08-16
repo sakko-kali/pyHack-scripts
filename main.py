c01d3e4dc7aca4001fb80adf50ce23810bc33af4
Sako-kali <sarkisssbarsegian@gmail.com>
2025-08-16 17:33:16 +0400

    day 3

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 
 
-def txt_info(txt):
-    with open(txt, "r", encoding="utf-8") as file:
-        arr = file.read().split()
+with open("logins.txt", "r", encoding="utf-8") as f_in, \
+     open("short_logins.txt", "w", encoding="utf-8") as f_out:
 
-    output=f"Количество слов: {str(len(arr))}\n"
-    seen = set()
-    for i in arr:
-        if arr.count(i) > 1 and i not in seen:
-            output+=f"{i} - {arr.count(i)}\n"
-            seen.add(i)
-    return output
-
-print(txt_info('111.txt'))
+    for line in f_in:
+        login = line.strip()  # убрали \n
+        if len(login) < 5:
+            f_out.write(login + "\n")
+            f_out.write(login + "\n")
