c66886e3e6dac3423453b4963b03ecd9d3886a9f
Sako-kali <sarkisssbarsegian@gmail.com>
2025-08-24 18:49:44 +0400

    day 9

--- new.py
+++ new.py
@@ -1,5 +1,32 @@
 import socket
+import os
+import subprocess
+from sys import stdin, stdout
 
-domain = "8.8.4.4"
-ip = socket.gethostbyaddr(domain)
-print(f"IP-адрес {domain}: {ip}")
+############################################Получить ip по домену#########################################
+# domain = "8.8.4.4"
+# ip = socket.gethostbyaddr(domain)
+# print(f"IP-адрес {domain}: {ip}")
+
+############################################Пинг по домену#########################################
+
+# hostname = "google.com"
+# resp = os.system(f"ping -c 4 {hostname}")
+# if resp == 0:
+#     print(f"{hostname} доступен")
+# else:
+#     print(f"{hostname} недоступен")
+
+############################################Пинг по домену через subprocess#########################################
+
+# hostname = "google.com"
+# comand = f"ping -c 4 {hostname}".split()
+# proc = subprocess.run(comand,capture_output=True, text=True)
+# for line in proc.stdout.splitlines():
+#     if "time=" in line:
+#         print(line)
+
+# p1 = subprocess.Popen(["ps","aux"], stdout=subprocess.PIPE, text=True)
+# p2 = subprocess.Popen(["grep","python"], stdin=p1.stdout, stdout=subprocess.PIPE, text=True)
+# output, _ = p2.communicate()
+# print(output)
